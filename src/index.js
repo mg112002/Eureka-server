7721b94b6ee1727d5ae514c9a5ccea50d000c157
mg112002 <mg900500@gmail.com>
2022-12-24 19:16:52 +0530

    v1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fs = require('fs')
-const { connect } = require('./data/init')
+const { connect } = require('../data/init')
 
 const PORT = process.env.PORT || 3000
 
