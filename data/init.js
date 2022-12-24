74d5044934d0b97e8996ba17ae802f05b2d847c1
mg112002 <mg900500@gmail.com>
2022-12-24 18:32:46 +0530

    v1

--- data/init.js
+++ data/init.js
@@ -1,4 +1,17 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb + srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+// mongoose.connect(`mongodb + srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 // link: `mongodb + srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1w0rped.mongodb.net/?retryWrites=true&w=majority`
+const connect = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+        console.log('connected to the db')
+    } catch (error) {
+        console.log(error.message)
+        throw error
+    }
+}
+
+module.exports = {
+    connect
+}
---
+++ src/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const { connect } = require('../data/init')
+
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.static(path.join(process.cwd(), 'public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+app.get('/api', (req, res) => {
+    res.end('Hello')
+})
+
+app.use(function (req, res, next) {
+    res.sendFile(path.join(process.cwd(), 'public', 'index.html'))
+})
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
+connect()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log("Server running on port", PORT)
+        })
+    })
+    .catch(error => {
+        console.log(error.message)
+        process.exit(1)
+    })
